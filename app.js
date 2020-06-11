40f8c6a32352f268d25d578275fe6b319d0607da
liivmarii <liv.alterskjaer@icloud.com>
2020-06-11 17:57:42 +0200

    const values selected from dom

--- app.js
+++ app.js
@@ -14,4 +14,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let newCell;
     for (newCell = 0; newCell < 200; newCell++) createCells()
 
+    // Tetromino code
+    const grid = document.querySelector('#tetris-container')
+    let cells = Array.from(document.querySelectorAll('#tetris-cell'))
+    const tetrominoWidth = 10
+
+    // Displaying the game score
+    const scoreDisplay = document.querySelector('#score')
+
+    // Start / pause game
+    const startPause = document.querySelector('#start-btn')
+
+
+
 })
