f12d2064bc58a22e06494a829d3e2a9e9594aef3
liivmarii <liv.alterskjaer@icloud.com>
2020-06-13 21:39:56 +0200

    eventhandling for moving tetrominoes

--- app.js
+++ app.js
@@ -67,4 +67,120 @@ document.addEventListener('DOMContentLoaded', () => {
         [1, width, width + 1, width * 2]
     ]
 
+    const theTetrominoes = [
+        iTetromino,
+        jTetromino,
+        lTetromino,
+        oTetromino,
+        sTetromino,
+        tTetromino,
+        zTetromino
+    ]
+
+    // Set tetromino start position
+    const INITIAL_POSITION = 4
+    let currentPosition = INITIAL_POSITION
+
+    // Set initial tetromino rotation
+    const INITIAL_ROTATION = 0
+    let currentRotation = INITIAL_ROTATION
+
+    // Select a random tetromino
+    let random = Math.floor(Math.random() * theTetrominoes.length)
+
+    let current = theTetrominoes[random][currentRotation]
+
+    // Draw or undraw a random tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    // Move current tetromino down every second
+    timeID = setInterval(moveDown, 1000)
+
+    // Assign functions to keyCodes
+    function control (e) {
+        if (e.keyCode === 37 || e.keyCode === 65) moveLeft()
+        if (e.keyCode === 39 || e.keyCode === 68) moveRight()
+        if (e. keyCode === 38 || e.keyCode === 87) rotate()
+        if (e.keyCode === 40 || e.keyCode == 83) moveDown()
+    }
+
+    document.addEventListener('keyup', control)
+
+    // Move down function
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    // Freeze current tetromino and draw a new one
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+            currentPosition = INITIAL_POSITION
+            currentRotation = INITIAL_ROTATION
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+
+            draw()
+        }
+    }
+
+    // Move tetromino left unless out of bounds
+    function moveLeft() {
+        undraw()
+
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if (!isAtLeftEdge) currentPosition -= 1
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    // Move tetromino right unless out of bounds
+    function moveRight() {
+        undraw()
+
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if (!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1
+        }
+
+        draw()
+    }
+
+    // Rotate tetromino
+    function rotate() {
+        undraw()
+
+        currentRotation ++
+
+        if(currentRotation === current.length) {
+            currentRotation = INITIAL_ROTATION
+        }
+
+        current = theTetrominoes[random][currentRotation]
+
+        draw()
+    }
+
 })
