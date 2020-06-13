5f7ea50e8ff997ec885917f60e721b9e5eaee368
liivmarii <liv.alterskjaer@icloud.com>
2020-06-13 18:09:02 +0200

    Tetromino shapes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const tetrisGrid = document.querySelector('#tetris-grid')
     const miniGrid = document.querySelector('#mini-grid')
     let square
-    let tetrisSquares = Array.from(document.querySelectorAll('#tetris-grid div'))
-    let miniSquares = Array.from(document.querySelectorAll('#mini-grid div'))
+    const tetrisSquares = Array.from(document.querySelectorAll('#tetris-grid div'))
+    const miniSquares = Array.from(document.querySelectorAll('#mini-grid div'))
     const scoreDisplay = document.querySelector('#score')
     const startBtn = document.querySelector('#start-btn')
     const width = 10
@@ -33,5 +33,65 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (square = 0; square < 16; square++) miniSquare()
 
+    // Tetromino shapes
+    const iTetromino = [
+        [0, width, width * 2, width * 3],
+        [0, width, width * 2, width * 3],
+        [0, width, width * 2, width * 3],
+        [0, width, width * 2, width * 3]
+    ]
 
+    const oTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+
+    const tTetromino = [
+        [0, width, width + 1, width * 2],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1],
+        [1, width, width + 1, width + 2]
+    ]
+
+    const jTetromino = [
+        [1, width + 1, width * 2, width * 2 + 1],
+        [0, width, width + 1, width + 2],
+        [1, 2, width + 1, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 2]
+    ]
+
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
+
+    const sTetromino = [
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1]
+    ]
+
+    const zTetromino = [
+        [width, width + 1, width * 2 + 1, width * 2 + 2],
+        [2, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width * 2 + 1, width * 2 + 2],
+        [2, width + 1, width + 2, width * 2 + 1]
+    ]
+
+    const tetrominoShapes = [
+        iTetromino,
+        oTetromino,
+        tTetromino,
+        jTetromino,
+        lTetromino,
+        sTetromino,
+        zTetromino
+    ]
+
+    //
 })
