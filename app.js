bb20854398b9d99cd1e91976ce6b82f32beb10df
liivmarii <liv.alterskjaer@icloud.com>
2020-06-13 19:52:11 +0200

    Populating tetris grid

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    // Populating tetris grid
+    for (let square = 0; square < 200; square++) {
+        let newSquare = document.createElement('div')
+        document.querySelector('.grid').appendChild(newSquare)
+    }
+
+    for (let square = 0; square < 10; square++) {
+        let newSquare = document.createElement('div')
+        newSquare.classList.add('taken')
+        document.querySelector('.grid').appendChild(newSquare)
+    }
+
 })
